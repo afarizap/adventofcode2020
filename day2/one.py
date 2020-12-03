588441c182ef6c1b7ffb9724f690931e2cce0301
afarizap <afarizap@gmail.com>
2020-12-03 01:49:10 +0000

    nice

--- day2/one.py
+++ day2/one.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+with open("input.txt") as f:
+    j = 0
+    for i in range(1000):
+        x = f.readline()
+        y = x.split()
+        a = y[0].split("-")
+        b = y[1].split(":")
+        c = y[2]
+        d = c.count(b[0])
+        if int(a[0]) <= d <= int(a[1]):
+            j += 1
+        # print(a[0], a[1], b[0], c, d, j)
+print(j)
